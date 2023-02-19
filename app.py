8e9a54195a805eb8d7278e4a7557f562a54dc629
Pavithra Shavanni <69794244+Pavithra-Nagraj@users.noreply.github.com>
2023-02-19 17:21:38 +0000

    added requirements text

--- app.py
+++ app.py
@@ -1,12 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  'id': 1,
+  'title': 'Frontend Developer',
+  'location': 'Bangalore',
+  'Salary': 'Rs 15000'
+}, {
+  'id': 2,
+  'title': 'Frontend Developer',
+  'location': 'Bangalore',
+  'Salary': 'Rs 15000'
+}, {
+  'id': 3,
+  'title': 'Frontend Developer',
+  'location': 'Bangalore',
+  'Salary': 'Rs 15000'
+}, {
+  'id': 4,
+  'title': 'Frontend Developer',
+  'location': 'Bangalore',
+  'Salary': 'Rs 15000'
+}]
+
+
 @app.route("/")
 def hello_word():
-  return "Hello World!"
+  return render_template('home.html', jobs=JOBS, company_name='JOVIAN')
+
 
 print(__name__)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
-  
